d25577c5f654bd2c82e28e8b358b769b19980cc7
gzimbric <gabe@zimbri.cc>
2018-08-16 15:18:11 -0500

    Initial Commit

---
+++ elements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  elements
+//
+//  Created by Gabe Zimbric on 8/16/18.
+//  Copyright © 2018 Gabe Zimbric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
