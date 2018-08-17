26f981b596103c6a27d7eff8e116bb768ecefd28
gzimbric <gabe@zimbri.cc>
2018-08-16 21:18:01 -0500

    init upload

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'elements' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+pod "ForecastIO"
+pod 'SwiftLocation', '~> 3.2.3'
+
+  # Pods for elements
+
+  target 'elementsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'elementsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- elements/ViewController.swift
+++ elements/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import ForecastIO
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var iconLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Large Navbar
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        //
+        let locManager = CLLocationManager()
+        var currentLocation: CLLocation!
+        let geoCoder = CLGeocoder()
+        let tempSymbol = "°"
+        // Request for location
+        locManager.requestWhenInUseAuthorization()
+        // Get location (lat/long)
+        if (CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways){
+            currentLocation = locManager.location
+            print(currentLocation.coordinate.latitude)
+            print(currentLocation.coordinate.longitude)
+        }
+        // Grab lat/long to find city
+        let location = CLLocation(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+        // Get weather data from Dark Sky using ForecastIO
+        let client = DarkSkyClient(apiKey: "lol")
+        client.units = .auto
+        client.language = .english
+        client.getForecast(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, excludeFields: [.alerts, .daily, .flags, .minutely]) { result in
+            switch result {
+            case .success(let forecast, let requestMetadata):
+                let temp = round(forecast.currently!.temperature!)
+                let infoText = forecast.currently!.summary!
+                // Find city based off of lat/long
+                geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+                    var placeMark: CLPlacemark!
+                    placeMark = placemarks?[0]
+                    if let city = placeMark.locality {
+                        // Setup view
+                        DispatchQueue.main.async(execute: {
+                            self.navigationController?.navigationBar.topItem?.title = String(temp.cleanValue) + tempSymbol + " " + city
+                            self.weatherIcon.image = UIImage(named: forecast.currently!.icon!.rawValue)
+                            self.weatherLabel.text = forecast.hourly!.summary!
+                            self.iconLabel.text = infoText.uppercased()
+                        })
+                        // Console logging
+                        print(city)
+                    }
+                })
+                // More console logging
+                print(forecast.currently!.temperature!)
+                print(forecast.currently!.icon!)
+                print(forecast.hourly!.summary!)
+                print(requestMetadata)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-
-
 }
-
+// Round temp and get rid of decimal
+extension Double {
+    var cleanValue: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
